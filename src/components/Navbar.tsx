51b50de8eae707e65afd360771eca8ba0119902d
luigyy <luigy.valverde@ucr.ac.cr>
2023-09-08 19:00:15 -0600

    added dropdown on hover to main navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
   return (
-    <div className="shadow">
+    <div className="border-b border-zinc-200 pb-2">
       <div className="relative flex h-16 items-center justify-between  px-[7%]">
         <div className="flex items-center gap-x-5">
           <Link href="#_">
@@ -39,11 +39,193 @@ const Navbar: React.FC<NavbarProps> = ({}) => {
           </Link>
         </div>
       </div>
-      <div className=" mx-auto flex h-7 items-center justify-center gap-x-14  ">
-        <button className="font-antonio text-sm">Ropa</button>
-        <button className="font-antonio text-sm">Deporte</button>
-        <button className="font-antonio text-sm">Zapatos</button>
-        <button className="font-antonio text-sm">Accesorios</button>
+      <div className="  mx-auto flex  h-7 items-center justify-center gap-x-14  ">
+        <div className="">
+          <button className="peer relative font-antonio text-sm hover:text-orange focus:text-orange">
+            Ropa
+          </button>
+          <div
+            className="absolute right-1/2 mt-[10px]  hidden translate-x-1/2 flex-col border border-zinc-200 bg-creme 
+         hover:flex  peer-hover:flex peer-focus:flex md:w-1/2"
+          >
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  font-semibold hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Ver todo
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Abrigos
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <button className="p peer font-antonio text-sm hover:text-orange focus:text-orange">
+            Deporte
+          </button>
+          <div
+            className="absolute right-1/2  mt-[10px] hidden translate-x-1/2 flex-col border border-zinc-200 bg-creme 
+         hover:flex  peer-hover:flex peer-focus:flex md:w-1/2"
+          >
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  font-semibold hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Ver todo
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Abrigos
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+          </div>
+        </div>
+        <div>
+          <button className="peer font-antonio text-sm hover:text-orange focus:text-orange">
+            Zapatos
+          </button>
+          <div
+            className="absolute right-1/2 mt-[10px] hidden translate-x-1/2 flex-col border border-zinc-200 bg-creme 
+         hover:flex  peer-hover:flex peer-focus:flex md:w-1/2"
+          >
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  font-semibold hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Ver todo
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Abrigos
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <button className="peer font-antonio text-sm hover:text-orange focus:text-orange">
+            Accesorios
+          </button>
+          <div
+            className="absolute right-1/2 mt-[10px] hidden translate-x-1/2 flex-col border border-zinc-200 
+         bg-creme  hover:flex peer-hover:flex peer-focus:flex md:w-1/2"
+          >
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  font-semibold hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Ver todo
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Abrigos
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+            <Link
+              className="px-5 py-2 font-quicksand text-xs  hover:-translate-y-[1px] hover:font-semibold hover:text-green"
+              href="#"
+            >
+              Jeans
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
